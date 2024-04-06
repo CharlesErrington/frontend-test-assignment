4a9fc9817d7e220ec73173ff357916f0c64cb969
Charles Errington <charleserringtondev@gmail.com>
2024-04-06 15:18:58 +0100

    test: implement unit test

---
+++ src/components/atoms/__tests__/CircularButton.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { CircularButton } from "../CircularButton";
+import { describe, it, expect, vi } from "vitest";
+
+describe("CircularButton", () => {
+  it("renders the button with the provided icon", () => {
+    const mockIcon = <div data-testid="button-icon" />;
+    render(
+      <CircularButton icon={mockIcon} onClick={() => console.log("Click")} />
+    );
+    expect(screen.getByTestId("button-icon")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("triggers on click function when clicked", () => {
+    const mockIcon = <div data-testid="button-icon" />;
+    const onClickMock = vi.fn();
+    render(<CircularButton icon={mockIcon} onClick={onClickMock} />);
+    const buttonElement = screen.getByRole("button");
+    fireEvent.click(buttonElement);
+
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
+});
