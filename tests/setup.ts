d1d7264477526a333262744ea0a97db83862b156
Charles Errington <charleserringtondev@gmail.com>
2024-04-07 14:47:21 +0100

    chore: add scripts for tests and correct import in vitest setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,4 @@
-/// <reference types="vitest" />
+import "vitest";
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
